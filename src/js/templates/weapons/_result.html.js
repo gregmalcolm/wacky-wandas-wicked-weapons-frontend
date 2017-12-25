af80a21f2d3ae331b8b2663225b3a89d60b9afb7
Greg Malcolm <greg@gregmalcolm.com>
2017-12-24 21:21:51 -0500

    Card style styling of results

--- src/js/templates/weapons/_result.html.js
+++ src/js/templates/weapons/_result.html.js
@@ -1,26 +1,28 @@
 const resultTemplate = (model) => `
     <li class="search-result" data-id="${model.id}">
-        <a class="result-details__title" href="#">
-            <h3 class="result-details__title-header">
-                ${model.name}
-            </h3>
-        </a>
-        <a href="#">
-            <img class="search-result__image" src="${model.imageUrl}" alt="${model.name}">
-        </a>
-        <dl class="result_details__info__specifics">
-            <dt>Costs:</dd>
-            <dd>8 <span class="gold-pieces">gp</span>
-            <dt>Damage:</dt>
-            <dd>1d4</dd>
+        <div class="search-details">
+            <a class="result-details__title" href="#">
+                <h3 class="result-details__title-header">
+                    ${model.name}
+                </h3>
+            </a>
+            <a href="#">
+                <img class="search-result__image" src="${model.imageUrl}" alt="${model.name}">
+            </a>
+            <dl class="result_details__info__specifics">
+                <dt>Costs:</dd>
+                <dd>8 <span class="gold-pieces">gp</span>
+                <dt>Damage:</dt>
+                <dd>1d4</dd>
 
-            <dt>Range:</dt>
-            <dd>10 Ft</dd>
+                <dt>Range:</dt>
+                <dd>10 Ft</dd>
 
-            <dt>Weight:</dt>
-            <dd>2 lb</dd>
-        </dl>
-        <button class="buy-button" type="button">Buy!</button>
+                <dt>Weight:</dt>
+                <dd>2 lb</dd>
+            </dl>
+            <button class="buy-button" type="button">Buy!</button>
+        </div>
     </li>
     `;
 export default resultTemplate;
