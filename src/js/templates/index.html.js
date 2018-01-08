2d4dd71c8885114ddb9268abe39a0d94609eb26e
Greg Malcolm <greg@gregmalcolm.com>
2018-01-07 22:01:46 -0500

    Got rid of dollars

--- src/js/templates/index.html.js
+++ src/js/templates/index.html.js
@@ -2,7 +2,7 @@ const indexTemplate = () => `
     <div class="welcome">
         <h2>Wacky Wanda's</h2>
         <h3>We put the whack back in wacky</h3>
-        <img src="/images/whacky.jpg" alt="whacky">
+        <img src="/images/whacky2.png" alt="whacky">
     </div>
 `;
 
