1f62815b1f619a3e0a2456a4d7be86edd97e6b5c
Greg Malcolm <greg@gregmalcolm.com>
2018-03-29 09:16:40 -0400

    Updated for post-codemash

--- src/js/templates/errors.html.js
+++ src/js/templates/errors.html.js
@@ -1,7 +1,7 @@
 const errorsTemplate = () => `
     <div class="errors-frame">
         <h3>Boom</h3>
-        <img src="/images/disappointed.jpg" alt="disappointed">
+        <img src="/images/disappointed-orc.jpg" alt="disappointed">
         <div class="footer-text">Skimped on the unit tests, didn't ya?</div>
     </div>
 `;
